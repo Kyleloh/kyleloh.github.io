6446f1274a230fb5c8b37ba43abfbc15f834e57a
kyle.lo <kyle.lo@vipshop.com>
2018-01-04 17:02:40 +0800

    update

--- loulan/wap/js/wap_index.js
+++ loulan/wap/js/wap_index.js
@@ -1,25 +1,14 @@
-var mySwiper = new Swiper ('.swiper-container', {
-    loop: true,
-    // 如果需要前进后退按钮
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',onSlideChangeEnd: function(swiper){
-      slideTo((swiper.activeIndex-1)%4, false);
-    }
-  })        
-
-$('.tab .item').click(function(){
-    slideTo($(this).index(), true);
+// 去到预约的地方
+$(".toBook").click(function(){
+    $('body,html').animate({scrollTop:$(".formArea").offset().top},500);
 });
 
-function slideTo(num, needScroll) {
-    $(".tab .item").removeClass("active").eq(num).addClass("active");
-    $(".focus-text .item").removeClass("active").eq(num).addClass("active");
-    if(needScroll){
-        mySwiper.slideTo(num+1);
-    }
-}
+// tab切换
+$('.tab .item').click(function(){
+    showPart($(this).index());
+});
 
-
+// 表单radio选项样式展示
 setInterval(function(){
     $(".formArea .radioArea label").each(function(){
         if($(this).find("input[type=radio]").eq(0).prop("checked")){
@@ -29,3 +18,30 @@ setInterval(function(){
         }
     });
 },100);
+
+
+
+function slideInit(num){
+    var mySwiper = new Swiper ('#focus-item-'+num+' .swiper-container', {
+        loop: true,
+        // 如果需要前进后退按钮
+        nextButton: '#focus-item-'+num+' .swiper-button-next',
+        prevButton: '#focus-item-'+num+' .swiper-button-prev',
+        onSlideChangeEnd: function(swiper){
+            $('#focus-item-'+num+' .focus-text .item').removeClass("active").eq(swiper.realIndex).addClass("active");
+        }
+    });
+}
+
+function showPart(num){
+    $(".tab .item").removeClass("active").eq(num).addClass("active");
+    $(".focus-atlas .focus-item").removeClass("active").eq(num).addClass("active");
+}
+
+slideInit(0);
+slideInit(1);
+slideInit(2);
+slideInit(3);
+
+
+
